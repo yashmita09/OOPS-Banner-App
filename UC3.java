8ce8772fac2d0d63a7feb99337f9d1d99d72be26
yashmita09 <yashmitanandanwar@gmail.com>
2026-03-03 08:53:14 +0530

    Print OOPS

---
+++ UC3.java
@@ -0,0 +1,13 @@
+public class UC3 {
+    public static void main(String[] var0) {
+    System.out.println(String.join(" ", "   ***   ", "   ***    ", "******   ", "   *****"));
+    System.out.println(String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **     "));
+    System.out.println(String.join(" ", "**     **", "**     **", "**     **", "** "     ));
+    System.out.println(String.join(" ", "**     **", "**     **", "**    ** ", " **     "));
+    System.out.println(String.join(" ", "**     **", "**     **", "******   ", "   ****  "));
+    System.out.println(String.join(" ", "**     **", "**     **", "**       ", "       **"));
+    System.out.println(String.join(" ", "**     **", "**     **", "**       ", "        **"));
+    System.out.println(String.join(" ", "**   **", "  **    ** ", "**        ", "      ** "));
+    System.out.println(String.join(" ", "   ***   ", "   ***   ", "**        ", " *****"  ));
+}
+}
