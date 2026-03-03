15e43d40d0a339931e8515ad3af206f006162645
yashmita09 <yashmitanandanwar@gmail.com>
2026-03-03 08:36:19 +0530

    Print OOPS

---
+++ UC2.java
@@ -0,0 +1,33 @@
+public class UC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                " ********  " + " ********  " + " ********  " + " ********  "
+        );
+
+        System.out.println(
+                "*        * " + "*        * " + "*        * " + "*        "
+        );
+
+        System.out.println(
+                "*        * " + "*        * " + "*        * " + "*        "
+        );
+
+        System.out.println(
+                "*        * " + "*        * " + " ********  " + " ********  "
+        );
+
+        System.out.println(
+                "*        * " + "*        * " + "*         " + "         * "
+        );
+
+        System.out.println(
+                "*        * " + "*        * " + "*         " + "         * "
+        );
+
+        System.out.println(
+                " ********  " + " ********  " + "*         " + "  ********  "
+        );
+    }
+}
